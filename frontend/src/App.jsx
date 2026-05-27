d387db04840ebbf484610a4da23d6b5c495bebf0
Aditya Gupta <adityag7800@gmail.com>
2026-05-27 15:16:38 +0530

    Now dashboard is prortected

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function Dashboard() {
+  const logout = () => {
+    localStorage.removeItem("token");
+    window.location.href = "/";
+  };
+
   return (
-    <div className="min-h-screen flex justify-center items-center bg-black text-white text-3xl">
-      Welcome to Adventure Game Dashboard ⚔️
+    <div className="min-h-screen flex flex-col justify-center items-center bg-black text-white">
+      <h1 className="text-3xl mb-6">
+        Welcome to Adventure Game Dashboard ⚔️
+      </h1>
+
+      <button
+        onClick={logout}
+        className="bg-red-500 px-6 py-3 rounded"
+      >
+        Logout
+      </button>
     </div>
   );
 }
@@ -16,7 +31,15 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
